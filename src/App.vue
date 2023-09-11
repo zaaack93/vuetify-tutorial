045fb1b909ea817e88d45abeb9757b2eda817ce9
bouzangad zakaria <bouzangadzakaria@gmail.com>
2023-09-11 22:34:33 +0100

    add layout

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,55 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <v-list
+          :lines="false"
+          nav
+        >
+        <v-list-subheader>Menu principale</v-list-subheader>
+
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            :value="item"
+            active-color="primary"
+          >
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+
+      <v-app-bar-actions>
+        <v-btn icon>
+          <v-icon>mdi-account</v-icon>
+        </v-btn>
+      </v-app-bar-actions>
+    </v-app-bar>
+
     <v-main>
-      <HelloWorld/>
+      <!--  -->
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
   },
 
   data: () => ({
-    //
+    drawer:true,
+    items: [
+        { text: 'Acceuil', icon: 'mdi-folder' },
+        { text: 'Page 2', icon: 'mdi-folder' }
+      ],
   }),
 }
 </script>
